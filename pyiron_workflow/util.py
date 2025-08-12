327dfead8ce8fc51ee00a7fdcf56d44549753553
liamhuber <liamhuber@greyhavensolutions.com>
2025-08-12 14:49:26 -0700

    Adapt LocalPostgres for CI
    Where the db is not local

--- pyiron_workflow/util.py
+++ pyiron_workflow/util.py
@@ -7,6 +7,8 @@ import time
 
 class LocalPostgres:
     def __init__(self):
+        self._ci_mode = os.environ.get('GITHUB_ACTIONS') == 'true'
+
         self.dbdir = str(
             (pathlib.Path(__file__).parent.parent / ".pycor" / "pgdata").absolute()
         )
@@ -17,6 +19,28 @@ class LocalPostgres:
         self.password = "none"
         self.host = "localhost"
 
+    def boot(self):
+        """Initialize and start the database (local) or connect to service container (CI)"""
+        if self._ci_mode:
+            print("CI mode - using service container PostgreSQL")
+            return
+
+        print("Local mode - booting LocalPostgres")
+        self._stop()
+        self._remove()
+        self._init()
+        self._start()
+
+    def cleanup(self):
+        """Stop and remove the database (local only)"""
+        if self._ci_mode:
+            print("CI mode - database cleanup handled by GitHub")
+            return
+
+        print("Local mode - cleaning up LocalPostgres")
+        self._stop()
+        self._remove()
+
     def _run(self, *args, check=True, capture_output=False):
         if pathlib.Path(self.dbdir).exists():
             print("Running", args)
@@ -26,7 +50,7 @@ class LocalPostgres:
         print("Skipped", args)
         return None
 
-    def init(self):
+    def _init(self):
         os.makedirs(self.dbdir, exist_ok=True)
         self._run("initdb", "-D", self.dbdir)
         self._run(
@@ -44,7 +68,7 @@ class LocalPostgres:
         self._run("createdb", "-p", self.port, "-O", self.user, self.db)
         self._run("pg_ctl", "-D", self.dbdir, "stop")
 
-    def start(self):
+    def _start(self):
         self._run(
             "pg_ctl",
             "-D",
@@ -56,13 +80,13 @@ class LocalPostgres:
             "start",
         )
 
-    def stop(self):
+    def _stop(self):
         self._run("pg_ctl", "-D", self.dbdir, "stop")
 
-    def status(self):
+    def _status(self):
         self._run("pg_ctl", "-D", self.dbdir, "status")
 
-    def remove(self):
+    def _remove(self):
         try:
             self._run("pg_ctl", "-D", self.dbdir, "status", capture_output=True)
             self._run("pg_ctl", "-D", self.dbdir, "stop")
