349725b8d84d767ce6a04deaea95ad6eda006a40
Liam Huber <liamhuber@greyhavensolutions.com>
2025-09-10 12:38:54 -0700

    Pyiron nodes inits (#113)
    * Alphabetize and clean __init__.py
    
    * Add __init__.py files to all submodules
    
    * Add remaining node files/modules to import

--- pyiron_core/pyiron_nodes/__init__.py
+++ pyiron_core/pyiron_nodes/__init__.py
@@ -1,14 +1,15 @@
-# from ._version import get_versions
-
-# __version__ = get_versions()["version"]
-
 from pyiron_core.pyiron_nodes import (
     atomistic,
+    controls,
     databases,
-    plotting,
+    dataframe,
+    electrochemistry,
+    executors,
+    experiment,
     graphs,
     math,
-    executors,
-    dataframe,
+    plotting,
+    scipy,
+    thermodynamics,
     utilities,
-)  # , continuum_fem, large_scale_atomistic
+)
