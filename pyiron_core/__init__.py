55a720688b754e1f12011fd60733a40ff02c78b3
liamhuber <liamhuber@greyhavensolutions.com>
2025-09-10 16:48:00 -0700

    Expose key user-facing pyiron_workflow stuff
    Right in the pyiron_core init

--- pyiron_core/__init__.py
+++ pyiron_core/__init__.py
@@ -1,1 +1,12 @@
 import pyiron_core.pyiron_nodes as nodes
+
+from pyiron_core.pyiron_workflow import (
+    Node,
+    Workflow,
+    as_function_node,
+    as_inp_dataclass_node,
+    as_out_dataclass_node,
+    as_macro_node,
+    get_node_from_path,
+)
+from pyiron_core.pyiron_workflow.api.gui import GuiGraph, PyironFlow
