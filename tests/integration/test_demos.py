f967c0386bbeb57381a0001889a154862b906e17
liamhuber <liamhuber@greyhavensolutions.com>
2025-09-13 07:18:25 -0700

    Download data per-test
    Instead of universally in the test class

--- tests/integration/test_demos.py
+++ tests/integration/test_demos.py
@@ -1,3 +1,4 @@
+import contextlib
 import pathlib
 import unittest
 import urllib
@@ -12,19 +13,15 @@ ALL_DEMOS = list(f.stem for f in DEMOS_DIR.glob("*.json"))
 
 
 class TestDemoWorkflows(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.to_download = {
-            "MgCaFreeEnergies.pckl.gz": "https://github.com/pyiron-workshop/DPG-tutorial-2025/raw/351eeca736cce45f9bc3bfca84ab05de049e38c2/data/MgCaFreeEnergies.pckl.gz"
-        }
-        for fname, url in cls.to_download.items():
+    @contextlib.contextmanager
+    def _download_then_delete(self, url: str) -> None:
+        fname = url.split("/")[-1]
+        try:
             with urllib.request.urlopen(url) as response:
                 with open(fname, "wb") as f:
                     f.write(response.read())
-
-    @classmethod
-    def tearDownClass(cls):
-        for fname in cls.to_download:
+            yield None
+        finally:
             pathlib.Path(fname).unlink()
 
     def test_loading(self):
@@ -35,41 +32,44 @@ class TestDemoWorkflows(unittest.TestCase):
         )
 
     def test_landau(self):
-        # Instantiate the gui like a user
-        pf = pc.PyironFlow(["landau"], workflow_path=DEMOS_DIR, load_from_compact=True)
+        with self._download_then_delete(
+                "https://github.com/pyiron-workshop/DPG-tutorial-2025/raw/351eeca736cce45f9bc3bfca84ab05de049e38c2/data/MgCaFreeEnergies.pckl.gz"
+        ) as _:
+            # Instantiate the gui like a user
+            pf = pc.PyironFlow(["landau"], workflow_path=DEMOS_DIR, load_from_compact=True)
 
-        # Mock a "run" click
-        pf.wf_widgets[0].on_value_change(
-            {"new": "run: CalcPhaseDiagram", "old": None, "name": "manual"}
-        )
+            # Mock a "run" click
+            pf.wf_widgets[0].on_value_change(
+                {"new": "run: CalcPhaseDiagram", "old": None, "name": "manual"}
+            )
 
-        # The result gets passed directly to a display widget, and is hard to parse
-        # As a fallback, inspect the underlying graph
-        output = pf.wf_widgets[0].graph.nodes["CalcPhaseDiagram"].node.outputs.df.value
-        tail = output.tail(3)
+            # The result gets passed directly to a display widget, and is hard to parse
+            # As a fallback, inspect the underlying graph
+            output = pf.wf_widgets[0].graph.nodes["CalcPhaseDiagram"].node.outputs.df.value
+            tail = output.tail(3)
 
-        # And compare it to results grabbed from a manual run where the plot is ok
-        expected = pd.DataFrame({
-            'T': [528.781028, 528.781028, 528.781028],
-            'phase': ['fcc Ca', 'liquid', 'Mg$_2$Ca'],  # Note the newline in the third entry
-            'phi': [-1.78757, -1.78757, -1.78757],
-            'mu': [-0.316953, -0.316953, -0.316953],
-            'c': [1.000000, 0.721217, 0.333333],
-            'stable': [True, True, True],
-            'border': [True, True, True],
-            'refined': ['delaunay-triple', 'delaunay-triple', 'delaunay-triple'],
-            'f': [-2.104523, -2.016162, -1.893221],
-            'f_excess': [0.000000, 0.029454, 0.070434]
-        })
+            # And compare it to results grabbed from a manual run where the plot is ok
+            expected = pd.DataFrame({
+                'T': [528.781028, 528.781028, 528.781028],
+                'phase': ['fcc Ca', 'liquid', 'Mg$_2$Ca'],  # Note the newline in the third entry
+                'phi': [-1.78757, -1.78757, -1.78757],
+                'mu': [-0.316953, -0.316953, -0.316953],
+                'c': [1.000000, 0.721217, 0.333333],
+                'stable': [True, True, True],
+                'border': [True, True, True],
+                'refined': ['delaunay-triple', 'delaunay-triple', 'delaunay-triple'],
+                'f': [-2.104523, -2.016162, -1.893221],
+                'f_excess': [0.000000, 0.029454, 0.070434]
+            })
 
 
-        expected_sorted = expected.sort_values(by=list(expected.columns)).reset_index(drop=True)
-        actual_sorted = tail.sort_values(by=list(tail.columns)).reset_index(drop=True)
+            expected_sorted = expected.sort_values(by=list(expected.columns)).reset_index(drop=True)
+            actual_sorted = tail.sort_values(by=list(tail.columns)).reset_index(drop=True)
 
-        pdt.assert_frame_equal(
-            expected_sorted,
-            actual_sorted,
-            rtol=1e-3,
-            atol=1e-4,
-            check_dtype=False
-        )
+            pdt.assert_frame_equal(
+                expected_sorted,
+                actual_sorted,
+                rtol=1e-3,
+                atol=1e-4,
+                check_dtype=False
+            )
