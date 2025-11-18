06c9f25eb70b86a2d34c5300688f635478e75f62
Marvin Poul <ponder@creshal.de>
2025-11-17 18:31:12 -0800

    Add the WaterPotential node back

--- pyiron_core/pyiron_nodes/electrochemistry/structure/equilibrate.py
+++ pyiron_core/pyiron_nodes/electrochemistry/structure/equilibrate.py
@@ -101,6 +101,66 @@ def Equilibrate(
 
 
 @as_function_node
+def WaterPotential(
+    metal: str = "Al",
+    metal_charge: float = 0.0,
+    neon_charge: float = 0.0,
+    epsilon: float = 0.102,
+    sigma: float = 3.188,
+):
+    import pandas
+
+    water_potential = pandas.DataFrame(
+        {
+            "Name": ["H2O_tip3p"],
+            "Filename": [[]],
+            "Model": ["TIP3P"],
+            "Species": [["H", "O", metal, "Ne"]],
+            "Config": [
+                [
+                    "# @potential_species H_O  ### species in potential\n",
+                    "# W.L. Jorgensen",
+                    "The Journal of Chemical Physics 79",
+                    "926 (1983); https://doi.org/10.1063/1.445869 \n",
+                    "#\n",
+                    "\n",
+                    "units      real\n",
+                    "dimension  3\n",
+                    "atom_style full\n",
+                    "\n",
+                    "# create groups ###\n",
+                    "group O type 2\n",
+                    "group H type 1\n",
+                    f"group {metal} type 3\n",
+                    "group Ne type 4\n",
+                    "\n",
+                    "## set charges - beside manually ###\n",
+                    "set group O charge -0.830\n",
+                    "set group H charge 0.415\n",
+                    f"set group {metal} charge {metal_charge}\n",
+                    f"set group Ne charge {neon_charge}\n",
+                    "\n",
+                    "### TIP3P Potential Parameters ###\n",
+                    "pair_style lj/cut/coul/long 10.0\n",
+                    "pair_coeff * * 0.000 0.000 \n",
+                    "pair_coeff 2 2 0.102 3.188 \n",
+                    "pair_coeff 2 3 {:.4} {:.4} \n".format(epsilon, sigma),
+                    "pair_coeff 2 4 {:.4} {:.4} \n".format(epsilon, sigma),
+                    "bond_style  harmonic\n",
+                    "bond_coeff  1 450 0.9572\n",
+                    "angle_style harmonic\n",
+                    "angle_coeff 1 55 104.52\n",
+                    "kspace_style pppm 1.0e-5   # final npt relaxation\n",
+                    "\n",
+                ]
+            ],
+        }
+    )
+
+    return water_potential
+
+
+@as_function_node
 def IonPotential(
     metal: str = "Al",
     metal_charge: float = 0.0,
