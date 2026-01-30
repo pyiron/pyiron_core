62aaacc6a68a0b3820a90e6c94fd8b381b36b7a1
ahmed abdelkawy <a.abdelkawy@mpie.de>
2026-01-30 16:22:53 +0100

    add server option and settings to submit job on the queue

--- pyiron_core/pyiron_nodes/electrochemistry/structure/equilibrate.py
+++ pyiron_core/pyiron_nodes/electrochemistry/structure/equilibrate.py
@@ -14,6 +14,7 @@ def Equilibrate(
     solvated_electrode,
     water_potential,
     parameters=None,
+    server=None, 
     store: bool = True,
 ) -> OutputCalcMD:
     """
@@ -78,7 +79,16 @@ def Equilibrate(
     j.potential = water_potential
     j.calc_md(**asdict(parameters))
 
+    if server is not None:
+        print(f'Server = {server.queue}, {server}, {server.cores}')
+        j.server.queue = server.queue
+        j.server.cores = server.cores
+
     j.run(delete_existing_job=True)
+
+    if server is not None:
+        pr.wait_for_job(j, interval_in_s = 5, max_iterations = 1000)
+
     job_out = j["output/generic"]
 
     from pyiron_core.pyiron_nodes.atomistic.calculator.data import OutputCalcMD
